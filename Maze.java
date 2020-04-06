74bc478ef9258692a76d91e8e2ec4728d34aaad4
Irfaan <m.dustmahomed@student.curtin.edu.au>
2020-04-07 02:56:02 +0400

    rename_vars changes updated

--- Maze.java
+++ Maze.java
@@ -79,16 +79,16 @@ public class Maze
             }
             else
             {
-                if(grid[currentRow][currentCol] == EMPTY)
+                if(grid[row][col] == EMPTY)
                 {
-                    grid[currentRow][currentCol] = VISITED;
+                    grid[row][col] = VISITED;
                 }
-                else if(grid[currentRow][currentCol] == WALL)
+                else if(grid[row][col] == WALL)
                 {
                     done = true;
                     System.out.println("what's up"); // Hit wall.
                 }
-                else if(grid[currentRow][currentCol] == END)
+                else if(grid[row][col] == END)
                 {
                     done = true;
                     solved = true;
